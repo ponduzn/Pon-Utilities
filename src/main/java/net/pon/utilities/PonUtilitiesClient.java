95c92aa3dc2cbc2ffb87045a9168f7975d23ba51
ponduzn <ruby_berg@hotmail.com>
2024-06-26 23:59:11 +0200

    First commit

---
+++ src/main/java/net/pon/utilities/PonUtilitiesClient.java
@@ -0,0 +1,11 @@
+package net.pon.utilities;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PonUtilitiesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        
+    }
+
+}
